68ba89d09553f532e932ce664bf2caf5474aa977
fajar <helpdesk.aplikasi6@kpk.go.id>
2023-03-02 11:02:59 +0700

    Menambahkan View Register (belum selesai)

---
+++ music-social-network-vue/src/views/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="Register">
+        <div class="v-full p-6 flex justify-center items-center">
+            <div class="bg-black p-8 shadow rounded mb-6">
+                <h1 class="mb-6 text-lg text-gray-100 font-thin">Let's Register</h1>
+                <div class="mb-4">
+                    <label for="" class="block uppercase tracking-wide text-xs font-bold mb-2 text-gray-100">
+                        First Name
+                    </label>
+                    <input type="text"
+                        class="appearance-none block w-full bg-white text-gray-700 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                    <span class="text-red-500">This is an error message.</span>
+                    <button class="block w-full bg-green-500 text-white rounded-sm py-3 text-sm tracking-wide"
+                        type="submit">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
